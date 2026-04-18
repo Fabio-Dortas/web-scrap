2e66307fe78a7d8c1618bd90efaa001793986d5a
Fabio-Dortas <fabiodortas7@gmail.com>
2026-04-18 18:22:51 -0300

    Enhance web scraping with error handling and rich output
    Refactor web scraping logic to include error handling and use rich library for output.

--- web-scrap.py
+++ web-scrap.py
@@ -1,17 +1,29 @@
-import requests
 from bs4 import BeautifulSoup
+from rich import print
+from rich.panel import Panel
+import requests
 
-# 1. Receber o link do usuário
+
+#1 Fazer requisição Url
 url = input("Digite o link da página que deseja fazer scraping: ")
 
-# 2. Fazer requisição HTTP
-response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+try:
+    # 2. Fazer requisição HTTP
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
     
     links = soup.find_all('a')
-    for link in links[:30]:  # limitar para não imprimir demais
-        print(link.text.strip(),)
-else:
-    print("Erro ao acessar a página.")
+    for link in links[:30]:  # 3. limitar para não imprimir demais
+        caixa = Panel(link.text.strip())
+        print(caixa) # 4. Exibe resultados organizados em caixas
+    else:
+        print("Erro ao acessar a página.")
+
+except requests.exceptions.RequestException as e:
+    print("Erro na requisição:", e) # 5. Tratamento de erros
+
+except Exception as e:
+    print("Erro inesperado:", e)
